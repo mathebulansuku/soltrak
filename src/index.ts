4b1f6c08c581e62a29e9ee6536d78afe082fe0a3
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-17 14:55:37 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import postgres from "postgres";
+import dotenv from "dotenv";
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+const db = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+db.connect();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:3000`);
+});
