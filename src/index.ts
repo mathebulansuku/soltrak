cf048dc2fa18179bf0c57b2b674345b4a0c60c7e
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-17 21:39:20 +0200

    Used axios to request that data

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import express from "express";
-import http from "http";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import cors from "cors";
-import postgres from "postgres";
+import { Client } from "pg";
 import dotenv from "dotenv";
+import axios from "axios";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+const DATA_URL =
+  "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiZwptKq43saOqcOSOQN8Trhs4p_D05HXIU_c9xzARMMbXvFL-saAYOK8_Rp0MvkVN5u9NPkv6d0jf09LrQib8KyEtbOQm8A_mVar7t6jr8TFWcm210P4VGIlbB6BbVolxV5wdnCDhm3N1jlZrlRcnSBHTXHDVpsxixxxJG29oBa-3eYnksGFH3cKASudVBxcoAqszgBoItisZ3aZ0sKTRk2yklmgf2fZAShRDXrS519zsTArEcI1T6wNoqSZsiTBuMPV7CesZYAy8ySX9AWimBT7ewIsnaUG1adoQnSb6fbYH0ODUNTJZgl-g7Eg&lib=MR_mt8Wmapn2W5zwbI-xTtMWO3py5UuMP";
 
 dotenv.config();
 
@@ -19,20 +17,21 @@ const db = new Client({
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
 });
-db.connect();
+db.connect()
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error("Database connection error:", err));
 
-app.use(
-  cors({
-    credentials: true,
-  })
-);
-
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
-
-const server = http.createServer(app);
-
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server running on http://localhost:3000`);
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(DATA_URL);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
