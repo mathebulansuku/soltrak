9c0d3ea87c0fc72317c7d41ddada3ecf41c9cc40
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-18 10:04:38 +0200

    Create type to standardize API data

--- src/postgres.ts
+++ src/postgres.ts
@@ -1,5 +1,16 @@
 import { Client } from "pg";
 
+type SolarData = {
+  timestamp: Date;
+  air_temp: number;
+  dni: number;
+  dhi: number;
+  relative_humidity: number;
+  surface_pressure: number;
+  wind_speed_10m: number;
+  pv_power_rooftop: number;
+};
+
 const db = new Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
