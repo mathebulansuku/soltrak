bd9528c6fe8e2efe3377d6395cfb09fbf5f7515c
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-18 10:07:59 +0200

    function to insert data into database

--- src/postgres.ts
+++ src/postgres.ts
@@ -16,9 +16,14 @@ const db = new Client({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  port: parseInt(process.env.DB_PORT),
 });
 
 db.connect().catch((err) => {
   console.error("Failed to connect to the database:", err);
 });
+
+export async function insertSolarData(data: SolarData) {
+  try {
+  } catch (error) {}
+}
