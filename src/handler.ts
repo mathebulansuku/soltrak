88ea0a1c181510f7f8b8615a7d7f9dc65bff34eb
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-18 10:51:24 +0200

    handler.ts complete

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,12 @@
+import express from "express";
 import { insertSolarData } from "./postgres";
 import axios from "axios";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = 3000;
+
+dotenv.config();
 
 const DATA_URL =
   "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLh5WAjIIUPnqTa-ud-83lt8H4uFjcy9tdSKm4UVWi2Si3KaO8XS6oCRaJOpHkSaEzLObdCLbzMs34w8YOo8g5-_puSVZ-Bc-xOC1Wue8B2b03Pwx-dh_AZ4xp8vYeOG3HqTFFK90VG0_lRLYQf1zSGTQVmxg5ikjzTlo7SiJduT6IUs-sL1cBGtW_3NeEhTQY_XeXEZz_E7zTiykpcvdO4o3Adnt6d_nN2RHddpLi7Jr5Okwy-JFO8ropgQwK26oshe5RW4l1D2wxX4qGeFFKTv8TkCgRN6B7j11SvNQxsSLYPLUKm73TFDsrX-cQ&lib=MR_mt8Wmapn2W5zwbI-xTtMWO3py5UuMP";
@@ -50,3 +57,7 @@ export const handler = async () => {
     };
   }
 };
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
